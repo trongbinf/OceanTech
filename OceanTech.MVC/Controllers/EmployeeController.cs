a4650bbe7801a8958ccae10a62cf410a7ac279b3
dungnguyen284 <realvietdung@gmail.com>
2025-03-21 08:25:58 +0700

    resolve confilct

---
+++ OceanTech.MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,86 @@
+﻿using BusinessLogicLayer.Services.Interfaces;
+using BusinessModels.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace OceanTech.MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _employeeService.GetEmployees();
+            return PartialView("_List", employees);
+        }
+
+        public IActionResult Create()
+        {
+            return PartialView("_Create");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                await _employeeService.CreateEmployee(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var employee = await _employeeService.GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _employeeService.UpdateEmployee(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var employee = await _employeeService.GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _employeeService.DeleteEmployee(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
